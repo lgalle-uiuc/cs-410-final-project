9b1c873880f2be4c29851ae7baebfeaf3885bc7b
Luke Galle <lukegalle@Lukes-MacBook-Pro.local>
2025-10-24 15:23:07 -0400

    @main:threading cleanup

---
+++ information-retrieval/faiss-impl.py
@@ -0,0 +1,4 @@
+import numpy as np
+import faiss
+
+# https://medium.com/@mrcoffeeai/faiss-vector-database-be3a9725172f
--- services/gmail.py
+++ services/gmail.py
@@ -18,7 +18,7 @@ def send_message(msg):
 
 # https://docs.python.org/3/library/email.message.html
 # https://stackoverflow.com/questions/5938890/setting-different-reply-to-message-in-python-email-smtplib
-def smtp_send_email(subject: str, body: str, reply_to: Optional[str] = None, references: Optional[str] = None):
+def smtp_send_email(subject: str, body: str, message_id_prefix: str, reply_to: Optional[str] = None, references: Optional[str] = None):
 
     username = os.getenv("GMAIL_EMAIL")        
 
@@ -28,7 +28,7 @@ def smtp_send_email(subject: str, body: str, reply_to: Optional[str] = None, ref
     msg["Subject"] = subject
 
     # generate random message id for mock threading
-    message_id = f"<{uuid.uuid4()}@example.com>"
+    message_id = f"<{message_id_prefix}-{uuid.uuid4()}@example.com>"
     msg["Message-ID"] = message_id
 
     if reply_to:
@@ -66,22 +66,29 @@ def retrieve_email():
 
         references = email_message['References']
 
+        thread_id_prefix = email_message['Message-Id'].strip("<>").split("-")[0]
+
         if references != None:
             references = references.split(' ')
 
         if references == None:
-            email_threads[email_message['Message-Id']] = build_full_email_from_email_message(email_message)
+            email_threads[thread_id_prefix] = build_full_email_from_email_message(email_message)
         else:
             for thread in email_threads:
-                if thread == references[-1]:
-                    email_threads[thread] = email_threads[thread] + build_full_email_from_email_message(email_message)
+                if thread == thread_id_prefix:
+                    email_threads[thread_id_prefix] = email_threads[thread_id_prefix] + build_full_email_from_email_message(email_message)
 
     print(email_threads)
         
     mail.close()
     mail.logout()
 
+
+# https://stackoverflow.com/questions/64377425/how-can-i-read-the-mail-body-of-a-mail-with-python
 def build_full_email_from_email_message(email_message):
     body = email_message.get_payload(decode=True).decode('utf-8').strip()
     body = body.replace('\n', ' ').replace('\r', ' ')
-    return f"{email_message['Subject']} {body}"
+
+    subject = str(email.header.make_header(email.header.decode_header(email_message['Subject'])))
+
+    return f"{subject} {body} "
