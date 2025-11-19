f220d3cab81d68132f5e5d4e40d497536902b8a1
Luke Galle <lukegalle@Lukes-MacBook-Pro.local>
2025-11-19 17:11:01 -0500

    @main:on to hybrid

--- ir/hybrid.py
+++ ir/hybrid.py
@@ -1,2 +1,10 @@
-def query(query: str, keyword_algorithm:str):
-    print("hybrid")
+from ir.semantic import query as semantic_query
+from ir.keyword import query as keyword_query
+
+def query(query: str, keyword_algorithm:str, results=3):
+    semantic_results = semantic_query(query, 5)
+    keyword_restuls = keyword_query(query, keyword_algorithm)
+
+    # goal is to find max in each, normalize each to a range between 0 and 1, add them together, and then return the top results
+    
+    
